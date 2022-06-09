7fd5e989c1f7a4f4094092402874731621d187dc
eiloppang <eiloppang@naver.com>
2022-06-09 21:14:50 +0900

    지혜와 합침 2

---
+++ 2022_TeamProject_Test/Assets/Script/PlayerState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerState : MonoBehaviour
+{
+    //플레이어 hp
+    public Slider plyerHP;
+    //플레이어 mp
+    public Slider playerMP;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
